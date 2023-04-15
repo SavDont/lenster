15c8ad7d438721536f4c2fbc9119cf8215486bf7
Savarn Dontamsetti <sav.dontamsetti@gmail.com>
2023-04-16 00:22:50 +0900

    Fix build

--- packages/ui/src/Select.tsx
+++ packages/ui/src/Select.tsx
@@ -14,8 +14,8 @@ export const Select = forwardRef<HTMLInputElement, SelectProps>(function Select(
   ref
 ) {
   return (
-    <Listbox className="w-full" value={value} onChange={onChange}>
-      <div className="relative mt-1">
+    <Listbox value={value} onChange={onChange}>
+      <div className="relative mt-1 w-full">
         <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
           <span className="block truncate">{value}</span>
           <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
