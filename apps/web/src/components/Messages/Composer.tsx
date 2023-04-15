c25190f5f89f44fd760e0c885ebc21754b94d709
Savarn Dontamsetti <sav.dontamsetti@gmail.com>
2023-04-15 13:35:25 +0900

    add modal for payments

--- apps/web/src/components/Messages/Composer.tsx
+++ apps/web/src/components/Messages/Composer.tsx
@@ -2,14 +2,14 @@ import useWindowSize from '@components/utils/hooks/useWindowSize';
 import { ArrowRightIcon } from '@heroicons/react/outline';
 import { Mixpanel } from '@lib/mixpanel';
 import { t, Trans } from '@lingui/macro';
+import clsx from 'clsx';
 import { MIN_WIDTH_DESKTOP } from 'data/constants';
 import type { FC } from 'react';
 import { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
 import { useMessagePersistStore } from 'src/store/message';
 import { MESSAGES } from 'src/tracking';
-import { Button, Input, Spinner } from 'ui';
-
+import { Button, Input, Modal, Spinner } from 'ui';
 interface ComposerProps {
   sendMessage: (message: string) => Promise<boolean>;
   conversationKey: string;
@@ -17,6 +17,10 @@ interface ComposerProps {
 }
 
 const Composer: FC<ComposerProps> = ({ sendMessage, conversationKey, disabledInput }) => {
+  const [showPaymentModal, setShowPaymentModal] = useState<boolean>(false);
+  const [paymentToken, setPaymentToken] = useState<string>('');
+  const [paymentAmount, setPaymentAmount] = useState<string>('');
+
   const [message, setMessage] = useState<string>('');
   const [sending, setSending] = useState<boolean>(false);
   const { width } = useWindowSize();
@@ -39,6 +43,10 @@ const Composer: FC<ComposerProps> = ({ sendMessage, conversationKey, disabledInp
       toast.error(t`Error sending message`);
     }
     setSending(false);
+  };
+
+  const paymentClick = () => {
+    setShowPaymentModal(true);
   };
 
   useEffect(() => {
@@ -66,6 +74,16 @@ const Composer: FC<ComposerProps> = ({ sendMessage, conversationKey, disabledInp
         onKeyDown={handleKeyDown}
         onChange={(event) => onChangeCallback(event.target.value)}
       />
+      <Button onClick={paymentClick} variant="primary" aria-label="Send/Receive Money">
+        <div className="flex items-center space-x-2">
+          {Number(width) > MIN_WIDTH_DESKTOP ? (
+            <span>
+              <Trans>$</Trans>
+            </span>
+          ) : null}
+        </div>
+      </Button>
+
       <Button disabled={!canSendMessage} onClick={handleSend} variant="primary" aria-label="Send message">
         <div className="flex items-center space-x-2">
           {Number(width) > MIN_WIDTH_DESKTOP ? (
@@ -77,6 +95,40 @@ const Composer: FC<ComposerProps> = ({ sendMessage, conversationKey, disabledInp
           {sending && <Spinner size="sm" className="h-5 w-5" />}
         </div>
       </Button>
+      <Modal
+        title={t`Send/Receive Tokens`}
+        size="sm"
+        show={showPaymentModal}
+        onClose={() => setShowPaymentModal(false)}
+      >
+        <div className="w-full px-4 pt-4">
+          <Trans>Token</Trans>
+          <div className="flex justify-center space-x-4 p-4">
+            <Input type="text" placeholder={t`Type a token address`} value={paymentToken} />
+          </div>
+          <Trans>Amount</Trans>
+          <div className="flex justify-center space-x-4 p-4">
+            <Input type="text" placeholder={t`Type amount of token`} value={paymentAmount} />
+          </div>
+
+          <div className="flex">
+            <div
+              className={clsx(
+                'text-brand m-2 ml-4 flex flex-1 cursor-pointer items-center justify-center rounded p-2 font-bold'
+              )}
+            >
+              <Button>Send</Button>
+            </div>
+            <div
+              className={clsx(
+                'text-brand m-2 ml-4 flex flex-1 cursor-pointer items-center justify-center rounded p-2 font-bold'
+              )}
+            >
+              <Button>Request</Button>
+            </div>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 };
