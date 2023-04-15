4e6e9b57c2f5c65182f21f7ac8d48be9dadbedac
Chu Ka-cheong <kc@enyksecurity.com>
2023-04-16 00:02:19 +0900

    Update payment request chat bubble when receiving a referenced receipt

--- apps/web/src/components/utils/hooks/useSendPaymentReceipt.tsx
+++ apps/web/src/components/utils/hooks/useSendPaymentReceipt.tsx
@@ -1,14 +1,31 @@
 import type { Conversation } from '@xmtp/xmtp-js';
 import { useCallback } from 'react';
 
-const formatPaymentReceipt = (amount: string, token: string, to: string, txHash: string | object) =>
-  `#Moonlight - [Name] paid you:\nAmount: ${amount}\nToken: ${token}\nAddress: ${to}\nTxHash: ${
+const formatPaymentReceipt = (
+  amount: string,
+  token: string,
+  to: string,
+  txHash: string | object,
+  reference?: string
+) => {
+  let s = `#Moonlight - [Name] paid you:\nAmount: ${amount}\nToken: ${token}\nAddress: ${to}\nTxHash: ${
     token === 'USDC' ? txHash.hash : txHash
   }`;
+  if (reference) {
+    s += `\nReference: ${reference}`;
+  }
+  return s;
+};
 
 const useSendPaymentReceipt = (conversation?: Conversation) => {
   const sendPaymentReceipt = useCallback(
-    async (amount: string, token: string, txHash: string, to?: string): Promise<boolean> => {
+    async (
+      amount: string,
+      token: string,
+      txHash: string,
+      to?: string,
+      reference?: string
+    ): Promise<boolean> => {
       if (!conversation) {
         return false;
       }
@@ -16,7 +33,7 @@ const useSendPaymentReceipt = (conversation?: Conversation) => {
         if (!to) {
           to = ''; // TODO
         }
-        const message = formatPaymentReceipt(amount, token, to, txHash);
+        const message = formatPaymentReceipt(amount, token, to, txHash, reference);
         await conversation.send(message);
       } catch (error) {
         return false;
